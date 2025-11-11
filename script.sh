c0af5f504acc3a90debf7f7f45cee26bb8185fb5
Jayaprakash-Paspula <134996192+Jayaprakash-Paspula@users.noreply.github.com>
2025-11-11 15:35:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,15 +9,15 @@ then
   exit 1
 fi
 
-PROJECT_KEY=$1 # provide your project key
-TOKEN=$2 # provide your Public REST API token
+ZEPHYR_PROJECT_KEY=$1 # provide your project key
+ZEPHYR_API_TOKEN=$2 # provide your Public REST API token
 
 # Please provide the correct URL according to the product you use
-URL="https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=${PROJECT_KEY}&autoCreateTestCases=false"
-# URL="https://prod-api.zephyr4jiracloud.com/v2/automations/executions/junit?projectKey=${PROJECT_KEY}&autoCreateTestCases=false"
+URL="https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=${ZEPHYR_PROJECT_KEY}&autoCreateTestCases=false"
+# URL="https://prod-api.zephyr4jiracloud.com/v2/automations/executions/junit?projectKey=${ZEPHYR_PROJECT_KEY}&autoCreateTestCases=false"
 
 mvn test
 
 zip -D ./target/junit_tests.zip ./target/surefire-reports/TEST*.xml
 
-curl -o -X POST -F "file=@target/junit_tests.zip" -H "Authorization: Bearer ${TOKEN}" $URL
+curl -o -X POST -F "file=@target/junit_tests.zip" -H "Authorization: Bearer ${ZEPHYR_API_TOKEN}" $URL
